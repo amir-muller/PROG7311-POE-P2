9b11e984f990419406eb2ea9be9151e9e3217c59
Amir-rb <st10443090@myemeris.edu.za>
2026-04-20 23:28:40 +0200

    add Client funtion: Create.cshtml

--- PROG7311 POE P2/Controllers/HomeController.cs
+++ PROG7311 POE P2/Controllers/HomeController.cs
@@ -1,23 +1,54 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using PROG7311_POE_P2.Data;
 using PROG7311_POE_P2.Models;
+using PROG7311_POE_P2.Models.Client;
+using System.Diagnostics;
 
 namespace PROG7311_POE_P2.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDBContext _context;
         private readonly ILogger<HomeController> _logger;
+        
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDBContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
+        // INDEX
         public IActionResult Index()
+        {
+            // store data from the db to a var to pass to the view
+            var data = _context.Clients.ToList();
+
+            // pass the data to the view
+            return View(data);
+        }
+
+        // CREATE
+        [HttpGet]
+        public IActionResult Create()
         {
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Clients.Add(client);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(client);
+        }
+
+        // PRIVIACY
         public IActionResult Privacy()
         {
             return View();
